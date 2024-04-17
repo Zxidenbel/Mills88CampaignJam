efd43eb1755b63851e8336ba55c1267435f8df39
Zxidenbel <fern.badenhop@gmail.com>
2024-04-16 21:40:42 -0400

    Update Day 3
    Finished DMS sprites.
    More work on custom features.
    Changed start room to be in LM.
    Changed diet to include corpses and increased maul damage.

--- src/Denizen/Plugin.cs
+++ src/Denizen/Plugin.cs
@@ -1,36 +1,54 @@
 ﻿using BepInEx;
 using Fisobs.Core;
-using SlugBase.Features;
 using Denizen.Objects;
+using SlugBase;
 
 namespace Denizen
 {
     [BepInDependency("slime-cubed.slugbase", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInDependency("fisobs", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin("Mills88CampaignJam.Denizen", "The Denizen", "0.0.1")]
+    [BepInPlugin("oefans.Denizen", "The Denizen", "0.2.0")]
     public class Plugin : BaseUnityPlugin
     {
-        public static readonly PlayerFeature<float> SwimSpeed = FeatureTypes.PlayerFloat("swim_speed");
         public static readonly PlacedObject.Type Boulder = new PlacedObject.Type("Boulder", true);
 
         void OnEnable()
         {
             On.Player.Update += Player_Update;
+            On.PlayerGraphics.Update += Player_Graphics_Update;
             Content.Register(new BoulderFisob());
+        }
+
+        private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
+        {
+            orig.Invoke(self, eu);
+            
+            if(SlugBaseCharacter.TryGet(self.slugcatStats.name, out SlugBaseCharacter _character))
+            {
+                if (_character.DisplayName == "The Denizen")
+                {
+                    if (self.bodyMode == Player.BodyModeIndex.Swimming)
+                    {
+                        self.swimForce += 0.8f;
+                    }
+                }
+            }
+        }
+
+        private void Player_Graphics_Update(On.PlayerGraphics.orig_Update orig, PlayerGraphics self)
+        {
+            orig.Invoke(self);
+            if (SlugBaseCharacter.TryGet(self.player.slugcatStats.name, out SlugBaseCharacter _character))
+            {
+                if (_character.DisplayName == "The Denizen")
+                {
+                    self.PlayerBlink();
+                }
+            }
         }
 
         void OnDisable()
         {
             On.Player.Update -= Player_Update;
-        }
-
-        private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
-        {
-            orig(self, true);
-            if (SwimSpeed.TryGet(self, out var swimSpeed))
-            {
-                // not implemented
-            }
         }
     }
 }
--- src/Denizen/obj/Debug/net4.8/Denizen.AssemblyInfo.cs
+++ src/Denizen/obj/Debug/net4.8/Denizen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Denizen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b119dae8d9623fafb7f83c655b6d1dc537ce52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44ee989d65cc7f7e844da0a4c4baa3ff05eb597")]
 [assembly: System.Reflection.AssemblyProductAttribute("Denizen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Denizen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
